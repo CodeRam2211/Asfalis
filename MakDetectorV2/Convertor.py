28988317c52959295e2977def0fcd5f0f4fc25bd
CodeRam2211 <49099980+CodeRam2211@users.noreply.github.com>
2020-07-04 08:44:36 +0530

    deleted files

--- MakDetectorV2/Convertor.py
+++
@@ -1,9 +0,0 @@
-
-from tensorflow import lite
-from tensorflow.keras.models import load_model
-
-model1 = load_model("a.h5")
-converter = lite.TFLiteConverter.from_keras_model(model1) # Your model's name
-model = converter.convert()
-file = open( 'model.tflite' , 'wb' )
-file.write( model )
